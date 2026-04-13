ee63a316f8054635a230c2f85408b295d2a75abd
wwg1122 <shinramyun_black@naver.com>
2026-04-13 16:15:46 +0900

    first

--- 웹크롤링_프로젝트/WebCrawlling.py
+++ 웹크롤링_프로젝트/WebCrawlling.py
@@ -6,23 +6,33 @@ from selenium import webdriver
 import time
 import pandas as pd
 
-def find_CoffeeBean_store(site) -> None:
+def find_CoffeeBean_store(information) -> None:
     URLs = 'https://www.coffeebeankorea.com/store/store.asp'
     wd = webdriver.Chrome() #크롬 브라우저
-    time.sleep(2) #페이지 연결 2초 기다림.
-    for i in range(1,10): # 매점 갯수
+    time.sleep(1) #페이지 연결 2초 기다림.
+
+
+
+    for i in range(1,410): # 매점 갯수
         wd.get(URLs)
-        time.sleep(1)
+        time.sleep(2)
         try:
             wd.execute_script(f"storePop2({i})")
             time.sleep(1)
             html = wd.page_source
             soup = BeautifulSoup(html, "html.parser")
-            name = soup.select("div.store_txt")
-            print(name)
+            name = soup.select("div.store_txt > h2")
+            print(name[0].text)
+            address = soup.select('div.store_txt > table.store_table > tbody > tr > td')
+            print(address[2].text)
+            information.append(name[0].text + ',' + address[2].text)
             pass
         except Exception as ex:
             print("웹브라우저 오류")
             pass
 
-find_CoffeeBean_store("커피빈")
+coffee_bean_data = []
+find_CoffeeBean_store(coffee_bean_data)
+pandas_form = pd.DataFrame(coffee_bean_data, columns = ('store_name', 'address'))
+pandas_form.to_csv('CoffeeBean_EXCEL.csv', index=False, encoding='cp949')
+pandas_form.to_csv('CoffeeBean_EXCEL.csv', index=False, encoding='utf-8')
